e730868bd844c8b9fd3e48159fef7c7b549c06e2
ELGCSystem <elgcsystem@gmail.com>
2022-12-12 18:24:36 -0300

    Video in home page

--- frontend/src/components/Filtro/Filtro.jsx
+++ frontend/src/components/Filtro/Filtro.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Contador } from '../../components';
+import { Contador } from '..';
 
 //<Contador name={name}/>
 
--- frontend/src/views/inicio/Hero/Hero.jsx
+++ frontend/src/views/inicio/Hero/Hero.jsx
@@ -1,36 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Select } from "../../../components";
+import { useEffect } from "react";
+import { Button, Select } from "../../../components/index.js";
+import ReactPlayer from 'react-player';
 import { heroButtons } from "./HeroButtons.js";
+import video from './Video360.mp4';
 import './Hero.css';
-import { useNavigate } from 'react-router-dom';
 
-const Hero = () => (
-  <section className='hero'>
-    <div className='hero__buttons'>
-      <h2>¡Encontrá tu lugar en el mundo!</h2>
-      {
-        heroButtons.map((button) => (
-          <Button
-            to={button.url}
-            type={button.type}
-            className={button.class}
-          >
-            {button.text}
-          </Button>
-        ))
-      }
+const Hero = () => {
 
-      <Select>
+  useEffect(() => {
+    const videoElement = document.querySelector("video");
+    videoElement.play(); 
+  }, []);
+
+  return (
+    <section className='hero'>
+
+        <div className="video-container">
+          <video
+            className="video"
+            src={video}
+            muted
+          />
+        </div>
+
+      <div className='hero__buttons'>
+        <h2>¡Encontrá tu lugar en el mundo!</h2>
+
         {
           heroButtons.map((button) => (
-            <option value={button.id}>
+            <Button
+              to={button.url}
+              type={button.type}
+              className={button.class}
+            >
               {button.text}
-            </option>
+            </Button>
           ))
         }
-      </Select>
-    </div>
-  </section>
-);
+
+        <Select>
+          {
+            heroButtons.map((button) => (
+              <option value={button.id}>
+                {button.text}
+              </option>
+            ))
+          }
+        </Select>
+      </div>
+    </section>
+  )
+};
 
 export default Hero;
